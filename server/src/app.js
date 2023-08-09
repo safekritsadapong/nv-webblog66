1c3165fc495783a6169b689023a44e58661b4140
Nattapon Kumyaito <nattaponk@gmail.com>
2023-08-09 10:57:43 +0700

    install express

---
+++ server/src/app.js
@@ -0,0 +1,11 @@
+let express = require('express')
+const app = express()
+
+app.get('/status', function (req,res){
+    res.send('Hello nodejs server belong to nattapon')
+})
+
+let port = 8081
+app.listen(port, function(){
+    console.log('server running on ' + port)
+})
