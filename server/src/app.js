4f20694dad1a95ea16b916f1aeefb04ab2799571
Nattapon Kumyaito <nattaponk@gmail.com>
2023-08-16 09:51:00 +0700

    finished ch04

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,10 @@
 let express = require('express')
+let bodyParser = require('body-parser')
+
 const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:true}))
 
 app.get('/status', function (req,res){
     res.send('Hello nodejs server belong to nattapon')
@@ -7,8 +12,28 @@ app.get('/status', function (req,res){
 
 app.get('/hello/:name', function (req,res) {
     console.log('hello - ' + req.params.name)
-    res.send('sey hello with ' + req.params.name)
+    res.send('sey hello with ' + req.params.name) 
    })
+
+app.get('/user/:userId', function(req,res){
+    res.send('ดูข้อมูลผู้ใช้'+ req.params.userId)
+})
+
+app.get('/users', function(req,res){
+    res.send('ดูข้อมูลผู้ใช้ทั้งหมด')
+})
+
+app.post('/user', function (req,res) {
+    res.send('สร้างผู้ใช้' + JSON.stringify(req.body))
+})
+
+app.put('/user/:userId', function(req, res){
+    res.send('แก้ไขข้อมูลผู้ใช้ ' + req.params.userId + " " + JSON.stringify(req.body.name))
+})
+
+app.delete('/user/:userId', function(req, res){
+    res.send('ลบข้อมูลผู้ใช้ ' + req.params.userId + " " + JSON.stringify(req.body.name))
+})
 
 let port = 8081
 app.listen(port, function(){
