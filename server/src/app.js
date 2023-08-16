2df424fb3d7006f5bfb9e94be8713331df945b0d
nattaponkum <nattaponk@nu.ac.th>
2023-08-16 10:41:51 +0700

    finished Ch05

--- server/src/app.js
+++ server/src/app.js
@@ -6,6 +6,8 @@ const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 
+require('./route')(app)
+
 app.get('/status', function (req,res){
     res.send('Hello nodejs server belong to nattapon')
 })
@@ -14,26 +16,6 @@ app.get('/hello/:name', function (req,res) {
     console.log('hello - ' + req.params.name)
     res.send('sey hello with ' + req.params.name) 
    })
-
-app.get('/user/:userId', function(req,res){
-    res.send('ดูข้อมูลผู้ใช้'+ req.params.userId)
-})
-
-app.get('/users', function(req,res){
-    res.send('ดูข้อมูลผู้ใช้ทั้งหมด')
-})
-
-app.post('/user', function (req,res) {
-    res.send('สร้างผู้ใช้' + JSON.stringify(req.body))
-})
-
-app.put('/user/:userId', function(req, res){
-    res.send('แก้ไขข้อมูลผู้ใช้ ' + req.params.userId + " " + JSON.stringify(req.body.name))
-})
-
-app.delete('/user/:userId', function(req, res){
-    res.send('ลบข้อมูลผู้ใช้ ' + req.params.userId + " " + JSON.stringify(req.body.name))
-})
 
 let port = 8081
 app.listen(port, function(){
